b3411bb10e901ecda5d3c14fb836b3d785cd27af
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-04-03 12:42:09 +0200

    Fix 404 error handling.

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       fs.readFile('./404.html', function(err, content) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end(content);
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(content);
+        return res.end();
       })
     } else {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       return res.end();
     }
-  })
+  });
 }).listen(8080);
