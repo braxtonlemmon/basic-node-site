a4fc34d83a42da49d9bf5966bdaff0785b11a064
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-04-03 12:16:30 +0200

    Initial commit. Correctly route http requests.

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  const filename = (q.pathname === '/') ? ('./index.html') : (`.${q.pathname}.html`);
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      fs.readFile('./404.html', function(err, content) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end(content);
+      })
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    }
+  })
+}).listen(8080);
